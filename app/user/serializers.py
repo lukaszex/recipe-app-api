17f7c1fd38b97302c79127654008a7bb6379270f
lukaszex <lukaszex1997@gmail.com>
2024-02-04 19:48:18 +0100

    Fix for linting

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -43,3 +43,4 @@ class AuthTokenSerializer(serializers.Serializer):
             raise serializers.ValidationError(msg, code='authorization')
         attrs['user'] = user
         return attrs
+    
